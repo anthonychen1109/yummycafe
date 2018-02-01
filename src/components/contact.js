186cd572cc8021cf42c29300f09be8a8ac4b8531
ac065455 <chen.anthony.1109@gmail.com>
2018-02-01 17:41:20 -0500

    fixed parallax scrolling speed

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,12 +6,12 @@ const Contact = () =>
     <div id="contact" className="contact">
       <div className="contact-location container">
         <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3020.428703385703!2d-77.85960328518996!3d40.79657224037531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89cea8a052a2975d%3A0xcadfb450f19633d!2s320+E+Calder+Way%2C+State+College%2C+PA+16801!5e0!3m2!1sen!2sus!4v1517368426532"
-        height="300px"
+        height="450px"
         width="100%"
         position="relative"
         allowFullScreen/>
       </div>
-      <div className="contact-info">
+      <div className="contact-info container">
         <div className="contact-box">
           <h2 className="contact-header">Contact</h2>
           <br />
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './static/css/styles.css';
 
 // COMPONENTS
 import App from './components/app';
+import Gallery from './components/gallery';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/gallery" component={Gallery} />
+    </div>
+  </BrowserRouter>
+  , document.getElementById('root'));
