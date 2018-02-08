2830638d950ce77ae4d2baa5b0aff0053e61d383
ac065455 <chen.anthony.1109@gmail.com>
2018-02-08 13:52:13 -0500

    added online order button

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -107,9 +107,8 @@ class Gallery extends Component {
       <div>
         <nav>
           <ul className="topnav" id="dropdownClick">
-            <li className="logo"><a href="">Yummy Cafe</a></li>
-            <li><Link to="/">Home</Link></li>
-            <li><a href="#">Order Online</a></li>
+            <li className="logo"><a href="/">Yummy Cafe</a></li>
+            <li><a data-glf-cuid="4d098e5d-5767-4109-ae57-d4a4cf86748d" data-glf-ruid="66483f67-fcc1-45de-9915-d6bc795c1e3a" href="#">Order Online</a></li>
             <li><a href="#contact">Contact</a></li>
             <li id="test" className="dropdownIcon">
               <a href="javascript:void(0);"
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -24,11 +24,10 @@ class Navbar extends Component {
       <nav>
         <ul className="topnav" id="dropdownClick">
           <li className="logo"><a href="">Yummy Cafe</a></li>
-          <li><a href="">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#menu">Menu</a></li>
-          <li><a href="#">Order Online</a></li>
-          <li><Link to={"/gallery"}>Gallery</Link></li>
+          <li><a data-glf-cuid="4d098e5d-5767-4109-ae57-d4a4cf86748d" data-glf-ruid="66483f67-fcc1-45de-9915-d6bc795c1e3a" href="#">Order Online</a></li>
+          <li><a href="/gallery">Gallery</a></li>
           <li><a href="#contact">Contact</a></li>
           <li id="test" className="dropdownIcon">
             <a href="javascript:void(0);"
